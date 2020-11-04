ed7e43fd78677fe6fa56dd041639a2574c83c579
Eduardo Sazan <69261696+Saz0n@users.noreply.github.com>
2020-11-04 11:18:47 -0300

    Update Atividade 02.py

--- Atividade 02.py
+++ Atividade 02.py
@@ -11,25 +11,25 @@ a =  ("()")
 b =  ("{ }" )
 c =  ("[]") 
 
-print(entrada)
+
 if (entrada in abc):
     print("A string insirida está alinhada")
-    True
+    return True
 elif (entrada in ab):
     print("A string insirida está alinhada")
-    True
+    return True
 elif (entrada in bc):
     print("A string insirida está alinhada")
     True
 elif (entrada in a):
     print("A string insirida está alinhada")
-    True
+    return True
 elif (entrada in b):
     print("A string insirida está alinhada")
-    True
+    return True
 elif (entrada in c):
     print("A string insirida está alinhada")
-    True
+    return True
 else:
     print("A string não esta alinhada")
-    False
+    return False
